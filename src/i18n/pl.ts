753b7f5ee9a73b94d0b59bb09efaf8ff3a9fdab6
aras-2003 <arek2003@gmail.com>
2026-09-05 00:40:36 +0200

    style: shorten language switch label to EN

--- src/i18n/pl.ts
+++ src/i18n/pl.ts
@@ -1,5 +1,5 @@
 export const pl = {
-  langName: 'Polski', switchLabel: 'English',
+  langName: 'Polski', switchLabel: 'EN',
   skipMain: 'Przejdź do głównej treści', skipNav: 'Przejdź do nawigacji',
   navLabel: 'Główna nawigacja', menu: 'Menu',
   nav: { home: 'Start', about: 'O mnie', oaf: 'OAF', work: 'Praktyka', writing: 'Perspektywa' },
